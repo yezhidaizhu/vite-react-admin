a5f01b46916c74dbda2c3961d4aaa1a4ab911bfa
jiye <sujiye@uworks-gd.cc>
2024-01-10 16:48:43 +0800

    chroe

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -18,25 +18,8 @@ export default function Index() {
   }, []);
 
   return (
-    <Form className=" w-[400px] m-auto pt-[20vh]">
-      {/* <Form.Item>
-        <Input size="large" prefix={<UserOutlined />} placeholder="账户" />
-      </Form.Item>
-
-      <Form.Item>
-        <Input
-          size="large"
-          prefix={<LockOutlined />}
-          type="password"
-          placeholder="密码"
-        />
-      </Form.Item>
-
-      <Button block size="large" type="primary" onClick={onLogin}>
-        登录
-      </Button> */}
-
+    <div>
       <Button onClick={() => loginWithRedirect()}>登录</Button>
-    </Form>
+    </div>
   );
 }
